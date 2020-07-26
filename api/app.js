c0c210b60d444035d6f85c23d6d6ce3e3a894066
bertamatu <info.berta@gmail.com>
2020-07-26 09:54:25 +0300

    express/scrap works

--- api/app.js
+++ api/app.js
@@ -4,31 +4,20 @@ const scraper = require("./scrapping");
 
 const app = express(); // -- returns an object of type express
 
-app.use(cors()); //that any front-end client could make a request
+app.use(cors()); // -- that any front-end client could make a request
 
-const port = process.env.PORT || 3000; // -- environment variable PORT or manual port
+const port = process.env.PORT || 3333; // -- environment variable PORT or manual port
 
 app.get("/", (req, res) => {
-  // res.send("response from ' / ' ");
   res.json({ message: "This is hello from app.js '/' route" });
 });
 
-app.get("/api/jobs", (req, res) => {
-  // res.send("/api/jobs response!!!");
-  scraper.searchJobs(req.params.jobTitle).then((jobs) => {
-    res.json(jobs);
-  });
-});
+// http://localhost:3000/search/SEARCH-TIME // -- works
 app.get("/search/:jobTitle", (req, res) => {
-  // res.send("/api/jobs response!!!");
   scraper.searchJobs(req.params.jobTitle).then((jobs) => {
     res.json(jobs);
   });
 });
-
-// app.get("/api/jobs/:jobID", (req, res) => {
-//   res.send("/api/jobs response!!!");
-// });
 
 app.listen(port, (error) => {
   if (error) {
@@ -36,3 +25,5 @@ app.listen(port, (error) => {
   }
   console.log(`Server listening on http://localhost: ${port}`);
 });
+
+module.exports = { app };
