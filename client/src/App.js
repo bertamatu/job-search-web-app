2a9282f54df2a9d4cfee5f0544a0900b3b02ebc0
bertamatu <info.berta@gmail.com>
2020-07-26 09:52:47 +0300

    app works

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div>
-      <h6>Hello from App.js </h6>
-    </div>
-  );
+const API_URL = "http://localhost:3333/search";
+
+export class App extends Component {
+  state = {
+    jobs: [],
+    searchQuery: "",
+  };
+
+  callApi = async (e) => {
+    e.preventDefault();
+    await fetch(`${API_URL}/${this.state.searchQuery}`)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          jobs: responseData,
+        });
+        console.log("responseData", responseData);
+      })
+      .catch((error) => {
+        console.log("Error fetch!", error);
+      });
+    console.log("JOBS", this.state.jobs);
+  };
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.callApi}>
+          <input
+            type="text"
+            placeholder="Job title..."
+            value={this.state.searchQuery}
+            onChange={(e) => this.setState({ searchQuery: e.target.value })}
+          />
+          <button type="submit">Search</button>
+        </form>
+        {this.state.jobs.slice(0, 8).map((job, index) => {
+          return (
+            <div key={index}>
+              {job.jobTitle} <br />
+              <a href={job.link}>Read more</a>
+              <hr />
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 export default App;
